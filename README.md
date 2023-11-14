8ffc3fc7335b9a81c4e81b840acbfe579452eab2
Stephan Koglin-Fischer <stephan.koglin-fischer@funzt.dev>
2023-11-14 16:15:23 +0100

    feat: working devcontainer and codesandbox

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # php-beginner
 Support code for the PHP beginner course
+
+
+## References
+
+https://github.com/microsoft/vscode-remote-try-php
+https://github.com/devcontainers/templates/tree/main/src/php-mariadb
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// FILEPATH: /workspaces/php/index.php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Beginners Course</title>
+</head>
+<body>
+    <h1>Welcome, <?php echo "Student" ?>!</h1>
+    <p>This is your dashboard.</p>
+    <p>
+      <ul>
+        <li>
+          <a href="info.php">PHP Info</a>
+        </li>
+      </ul>
+    </p>
+</body>
+</html>
